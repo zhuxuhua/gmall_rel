14fe31e87f2fd83ea47450a7853c7dd49ca5233b
luckyzhu <1013905578@qq.com>
2020-05-25 17:41:25 +0800

    dubbo整合

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# gmall-zhu 本地修改版本
+# gmall0105 本地修改版本
+# gmall-user用户服务8080
+
+gmall-user-service用户服务的service层8070
+
+gmall-user-web用户服务的web层8080~~~~
---
+++ gmall-user-service/src/main/java/com/atguigu/gmall/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,39 @@
+package com.atguigu.gmall.user.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.atguigu.gmall.bean.UmsMember;
+import com.atguigu.gmall.bean.UmsMemberReceiveAddress;
+import com.atguigu.gmall.service.UserService;
+import com.atguigu.gmall.user.mapper.UmsMemberReceiveAddressMapper;
+import com.atguigu.gmall.user.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserMapper userMapper;
+
+    @Autowired
+    UmsMemberReceiveAddressMapper umsMemberReceiveAddressMapper;
+
+    @Override
+    public List<UmsMember> getAllUser() {
+
+        List<UmsMember> umsMemberList = userMapper.selectAll();//userMapper.selectAllUser();
+
+        return umsMemberList;
+    }
+
+    @Override
+    public List<UmsMemberReceiveAddress> getReceiveAddressByMemberId(String memberId) {
+        Example example = new Example(UmsMemberReceiveAddress.class);
+        example.createCriteria().andEqualTo("memberId",memberId);
+        List<UmsMemberReceiveAddress> umsMemberReceiveAddresses = umsMemberReceiveAddressMapper.selectByExample(example);
+        return umsMemberReceiveAddresses;
+    }
+}
+
